66e54225fa9ee03abbe29a7e18129ebc6160575f
Zuri Hazlehurst <zuri.hazle@gmail.com>
2025-04-30 21:56:53 +1200

    Filter products

--- addProducts/addProducts.js
+++ addProducts/addProducts.js
@@ -2,11 +2,61 @@ function createProduct(event) {
     event.preventDefault();
     var productName = document.getElementById("title").value;
     var productPrice = document.getElementById("price").value;
+    var productSize = document.getElementById("size").value;
+    var productCategory = document.getElementById("category").value;
+    var productColour1 = document.getElementById("colour1").value;
+    var productColour2 = document.getElementById("colour2").value;
+
     console.log('title: ' + productName + ', price: ' + productPrice);
 
     let newProductRef = firebase.database().ref('products').push();
-    newProductRef.set({
-    name: productName,
-    price: productPrice
+        newProductRef.set({
+        name: productName,
+        price: productPrice,
+        size: productSize,
+        category: productCategory,
+        colour1: productColour1,
+        colour2: productColour2
     });
+
+    var productID = newProductRef.key;
+    const file = document.getElementById("main_image").files[0];
+
+    if (file) {
+        const storageRef = firebase.storage().ref("product_images/" + productID + "_" + file.name);
+        storageRef.put(file).then(snapshot => {
+            return snapshot.ref.getDownloadURL(); 
+            }).then(downloadURL => {
+            console.log(downloadURL)
+            return firebase.database().ref("products/" + productID).set({
+                mainImage: downloadURL 
+            });
+            }).then(() => {
+            console.log("Product uploaded with image.");
+            }).catch(error => {
+            console.error("Upload failed:", error);
+            });
+    }
+
+    const imageArray = document.getElementById("images").files;
+    if (imageArray) {
+        for (i = 0; i < imageArray.length; i++) {
+            let imageNum = 'image' + i;
+            console.log(imageNum)
+            console.log(imageArray[i]);
+            const storageRef = firebase.storage().ref("product_images/" + productID + "_" + imageArray[i].name);
+            storageRef.put(imageArray[i]).then(snapshot => {
+                return snapshot.ref.getDownloadURL(); // Get public image URL
+            }).then(downloadURL => {
+                return firebase.database().ref("products/" + productID).update({
+                    [imageNum]: downloadURL 
+                });
+            }).then(() => {
+                console.log("Images added.");
+            }).catch(error => {
+                console.error("Extra images upload failed:", error);
+            });  
+        }
+    }
 }
+//add file type validation
--- shopAll/shopAll.js
+++ shopAll/shopAll.js
@@ -13,14 +13,12 @@ function displayProducts() {
         productTable.innerHTML = '';
     
         for (i = 0; i < productsArray.length; i++) {
-            if (productsArray[i].value.join == 'y') {
-                console.log(productsArray[i].value.p1Name + 's lobby is full')
-            } else {
                 var row = `
             <tr> 
                 <td>${productsArray[i].key}</td> 
                 <td>${productsArray[i].value.name}</td> 
-                <td>${productsArray[i].value.price}
+                <td>${productsArray[i].value.price}</td>
+               <td><img src="${productsArray[i].value.mainImage}"></td>
                 <td><button class="joinButton" onclick="goToPage(
                     '${productsArray[i].key}',
                     '${productsArray[i].value.name}',
@@ -28,15 +26,15 @@ function displayProducts() {
                     )">see more</button></td>
                 </tr>`
             productTable.innerHTML += row;
-            }
         };
     }, fb_error);
 }
 
-function goToPage(productID, productName, productPrice) {
+function goToPage(productID, productName, productPrice, productImage) {
     sessionStorage.setItem("productID", productID)
     sessionStorage.setItem("productName", productName)
     sessionStorage.setItem("productPrice", productPrice)
+    sessionStorage.setItem("productImage", productImage)
     window.location = "productPage.html"
 }
 
@@ -49,7 +47,92 @@ function displayProductPage() {
     priceDiv.innerHTML += productPrice;
 }
 
+function filterCategory(filter) {
+    firebase.database().ref('/products/').once('value', function(snapshot) {
+        var productsArray = [];
+        console.log(productsArray);
+        snapshot.forEach(function(childSnapshot) {
+            productsArray.push({
+                key: childSnapshot.key,
+                value: childSnapshot.val()
+            });
+        });
+    
+        var productTable = document.getElementById('productTable');
+        productTable.innerHTML = '';
+    
+        for (i = 0; i < productsArray.length; i++) {
+            if (productsArray[i].value.category === filter) {
+                var row = `
+                <tr> 
+                    <td>${productsArray[i].key}</td> 
+                    <td>${productsArray[i].value.name}</td> 
+                    <td>${productsArray[i].value.price}</td>
+                    <td><img src="${productsArray[i].value.mainImage}"></td>
+                    <td><button class="joinButton" onclick="goToPage(
+                        '${productsArray[i].key}',
+                        '${productsArray[i].value.name}',
+                        '${productsArray[i].value.price}'
+                        )">see more</button></td>
+                </tr>`
+            productTable.innerHTML += row;
+            }
+                
+        };
+    }, fb_error);
+}
+
+function filterSettings(event) {
+    event.preventDefault();
+    var colourFilter = document.getElementById("colourDropdown").value;
+    var sizeFilter = document.getElementById("sizeDropdown").value;
+    console.log('colour: ' + colourFilter);
+    console.log('size: ' + sizeFilter);
+
+    firebase.database().ref('/products/').once('value', function(snapshot) {
+        var productsArray = [];
+        console.log(productsArray);
+        snapshot.forEach(function(childSnapshot) {
+            productsArray.push({
+                key: childSnapshot.key,
+                value: childSnapshot.val()
+            });
+        });
+    
+        var productTable = document.getElementById('productTable');
+        productTable.innerHTML = '';
+
+        console.log("Current filters:", colourFilter, sizeFilter);
+    
+        for (i = 0; i < productsArray.length; i++) {
+            console.log(productsArray[i].value.colour1);
+            console.log(productsArray[i].value.size);
+            if ((colourFilter === 'all' || 
+                 productsArray[i].value.colour1 === colourFilter || 
+                 productsArray[i].value.colour2 === colourFilter) && 
+                (sizeFilter === 'all' || 
+                 productsArray[i].value.size === sizeFilter)) {
+                var row = `
+                <tr> 
+                    <td>${productsArray[i].key}</td> 
+                    <td>${productsArray[i].value.name}</td> 
+                    <td>${productsArray[i].value.price}</td>
+                    <td><img src="${productsArray[i].value.mainImage}"></td>
+                    <td><button class="joinButton" onclick="goToPage(
+                        '${productsArray[i].key}',
+                        '${productsArray[i].value.name}',
+                        '${productsArray[i].value.price}'
+                        )">see more</button></td>
+                </tr>`
+            productTable.innerHTML += row;
+            }
+                
+        };
+    }, fb_error);
+}
+
 function fb_error(error) {
     console.log("fb_error");
     console.log(error);
 }
+
