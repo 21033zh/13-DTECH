1466b574652eb97ce05da0e2926cfc7ea1435297
Zuri Hazlehurst <zuri.hazle@gmail.com>
2025-06-24 12:21:04 +1200

    write review, and read + display reviews on page from database

--- reviews/reviews.js
+++ reviews/reviews.js
@@ -26,25 +26,21 @@ function checkReview(event) {
 
             const imageArray = document.getElementById("images").files;
             if (imageArray) {
-                statusDiv = document.getElementById("status2")
-                statusDiv.innerHTML = 'Uploading images...'
                 for (i = 0; i < imageArray.length; i++) {
                     let imageNum = 'image' + i;
                     console.log(imageNum)
                     console.log(imageArray[i]);
-                    const storageRef = firebase.storage().ref("product_images/" + productID + "_" + imageArray[i].name);
+                    const storageRef = firebase.storage().ref("/reviews_images/" + imageArray[i].name);
                     storageRef.put(imageArray[i]).then(snapshot => {
                         return snapshot.ref.getDownloadURL(); // Get public image URL
                     }).then(downloadURL => {
-                        return firebase.database().ref("products/" + productID).update({
+                        return firebase.database().ref("reviews/" + uid).update({
                             [imageNum]: downloadURL 
                         });
                     }).then(() => {
                         console.log("Images added.");
-                        statusDiv.innerHTML = 'Images uploaded successfully...'
                     }).catch(error => {
                         console.error("Extra images upload failed:", error);
-                        statusDiv.innerHTML = 'Images upload failed'
                     });  
                 }
             }
@@ -56,4 +52,53 @@ function checkReview(event) {
         }
     });
     
+}
+
+
+
+
+
+function displayReviews() {
+    reviewsArray = [];
+    firebase.database().ref('/reviews/').once('value', function(snapshot) {
+        console.log(reviewsArray);
+        snapshot.forEach(function(childSnapshot) {
+            reviewsArray.push({
+                key: childSnapshot.key,
+                value: childSnapshot.val()
+            });
+        });
+        allReviewsArray = [].concat(reviewsArray);
+        createGrid(reviewsArray);
+});
+}
+
+function createGrid(reviewsArray) {
+    console.log(reviewsArray)
+    document.getElementById("reviewsContainer").innerHTML = '';
+    for (i = 0; i < reviewsArray.length; i++) {
+        appendProduct(
+            reviewsArray[i].value.image0,
+            reviewsArray[i].value.textReview,
+            reviewsArray[i].value.stars,
+            reviewsArray[i].key,
+        );
+    };
+}
+
+function appendProduct(image, text, stars, uid) {
+    let allInfo = [
+        image,
+        text,
+        stars,
+        uid
+    ]
+    const review = 
+            `<div class="reviewDiv">
+                <img class="productImage" src="${image}")">
+            <p class="stars">${stars}</p>
+            <p class="text" >size ${text}</p>
+            <p class="uid">$${uid}</p>
+            </div>`;
+    document.getElementById("reviewsContainer").innerHTML += review;
 }
