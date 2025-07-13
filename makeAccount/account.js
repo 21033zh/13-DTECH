7802cb702298f254d4dda173662ebb30e983c830
Zuri Hazlehurst <zuri.hazle@gmail.com>
2025-07-13 14:41:41 +1200

    account details layout

--- makeAccount/account.js
+++ makeAccount/account.js
@@ -3,12 +3,24 @@ function checkSignIn() {
         if (user) {
             // User is signed in.
             console.log("User is signed in:", user);
+            const displayNameArray = user.displayName.split(" ");
+            const firstName = displayNameArray[0];
+            const lastName = displayNameArray[1];
+            const email = user.email;
+            document.getElementById("p_welcomeName").innerHTML = firstName;
+            populateAccountInfo(firstName, lastName, email);
         } else {
             // User is signed out.
             console.log("user signed out")
             window.location="makeAccount.html"
         }
     });
+}
+
+function populateAccountInfo(firstName, lastName, email) {
+    document.getElementById("p_firstName").innerHTML = firstName;
+    document.getElementById("p_lastName").innerHTML = lastName;
+    document.getElementById("p_email").innerHTML = email;
 }
 
 function signOut() {
